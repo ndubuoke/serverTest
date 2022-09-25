3e75029056f0bc7daf18d2fe1759cf04042cf188
Ibrahim Adekunle <adefemi101@gmail.com>
2022-09-25 23:11:41 +0100

    added a connection options for mongoose.connect

--- config/mongo.js
+++ config/mongo.js
@@ -3,15 +3,22 @@ import chalk from "chalk";
 
 import { MONGO_URI } from "./config.js";
 
-export const mongoConfig = mongoose.connect(MONGO_URI).then(
-  () => {
-    console.log(`${chalk.bgBlue.white("Connected to the MongoDB database!")}`);
-  },
-  (err) => {
-    console.log(
-      `${chalk.bgRedBright.white("Cannot connect to the MongoDB database!")}`,
-      err
-    );
-    process.exit();
-  }
-);
+export const mongoConfig = mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    () => {
+      console.log(
+        `${chalk.bgBlue.white("Connected to the MongoDB database!")}`
+      );
+    },
+    (err) => {
+      console.log(
+        `${chalk.bgRedBright.white("Cannot connect to the MongoDB database!")}`,
+        err
+      );
+      process.exit();
+    }
+  );
