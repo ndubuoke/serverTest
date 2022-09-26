ee972d667de1edb1135668de9c7a5d4c922b7838
Ibrahim Adekunle <adefemi101@gmail.com>
2022-09-26 13:09:25 +0100

    update form model

--- models/form.js
+++ models/form.js
@@ -2,49 +2,49 @@ import mongoose, { Schema } from "mongoose";
 
 const formSchema = new mongoose.Schema(
   {
-      name: {
-        type: String,
-        required: true,
-      },
-      description: {
-        type: String,
-      },
-      formNavigation:  { 
-        type: String,
-        required: true,
-        enum: ["tabs", "wizard", "accordion", "single-page"],
-      },
-      builtFormMetadata:{ 
-        type: Object,
-        required: true,
-      },
-      createdBy:{ 
-        type: String,
-        required: true,
-     },
-      createdById :{ 
-          type: String,
-          required: true,
-      },
-      lastModifiedBy:{ 
-        type: String,
-        required: true,
-      },
-      lastModifiedById :{ 
-        type: String,
-        required: true,
-      },
-      status :{ 
-        type: String,
-        required: true,
-        default: "enabled",
-        enum: ["enabled", "disabled"],
-      },
-      statusDesc :{ 
-        type: String,
-        required: true,
-        default: "The record is enabled",
-      }, 
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    formNavigation: {
+      type: String,
+      required: true,
+      enum: ["tabs", "wizard", "accordion", "single-page"],
+    },
+    builtFormMetadata: {
+      type: Object,
+      required: true,
+    },
+    createdBy: {
+      type: String,
+      required: true,
+    },
+    createdById: {
+      type: String,
+      required: true,
+    },
+    lastModifiedBy: {
+      type: String,
+      required: true,
+    },
+    lastModifiedById: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: "enabled",
+      enum: ["enabled", "disabled", "edit-mode"],
+    },
+    statusDesc: {
+      type: String,
+      required: true,
+      default: "The record is enabled",
+    },
   },
 
   {
@@ -52,9 +52,6 @@ const formSchema = new mongoose.Schema(
   }
 );
 
-const Form = mongoose.model(
-  "form",
-  formSchema
-);
+const Form = mongoose.model("form", formSchema);
 
 export { Form };
