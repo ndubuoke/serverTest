f10e2d99e0a7540b2a064593b6a8926888a25ae5
Ibrahim Adekunle <adefemi101@gmail.com>
2022-09-27 14:28:15 +0100

    fix spelling

--- models/form-item-property.js
+++ models/form-item-property.js
@@ -2,54 +2,54 @@ import mongoose, { Schema } from "mongoose";
 
 const formItemPropertySchema = new mongoose.Schema(
   {
-      name: {
-        type: String,
-        required: true,
-      },
-      description: {
-        type: String,
-      },
-      value: {
-        type: String,
-        required: true,
-      },
-      formItemId:  { 
-        type: Schema.Types.ObjectId,
-         ref: 'formitem',
-         required: true,
-      },
-      controPropertyId:  { 
-        type: Schema.Types.ObjectId,
-        ref: 'controlproperty',
-        required: true,
-      },
-      createdBy:{ 
-        type: String,
-        required: true,
-     },
-      createdById :{ 
-          type: String,
-          required: true,
-      },
-      lastModifiedBy:{ 
-        type: String,
-        required: true,
-      },
-      lastModifiedById :{ 
-        type: String,
-        required: true,
-      },
-      status :{ 
-        type: String,
-        required: true,
-        default: "enabled",
-        enum: ["enabled", "disabled"],
-      },
-      statusDesc :{ 
-        type: String,
-        required: true,
-        default: "The record is enabled",
-      }, 
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+    formItemId: {
+      type: Schema.Types.ObjectId,
+      ref: "formitem",
+      required: true,
+    },
+    controlPropertyId: {
+      type: Schema.Types.ObjectId,
+      ref: "controlproperty",
+      required: true,
+    },
+    createdBy: {
+      type: String,
+      required: true,
+    },
+    createdById: {
+      type: String,
+      required: true,
+    },
+    lastModifiedBy: {
+      type: String,
+      required: true,
+    },
+    lastModifiedById: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: "enabled",
+      enum: ["enabled", "disabled"],
+    },
+    statusDesc: {
+      type: String,
+      required: true,
+      default: "The record is enabled",
+    },
   },
 
   {
